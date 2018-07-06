9909c13d7b76d5afcbd0ed99f43fc9d6b7406ac1
Rafał Panasiuk <panasiukraf@gmail.com>
2018-07-06 02:37:59 +0200

    Merged index.css with App.scss.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import configureStore from './store.js';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
